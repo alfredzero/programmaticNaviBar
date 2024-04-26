06916e8c671e8a9e02e15f47e5337bbbcf6f0ff4
alfredzero <amezcuaalfredo81@gmail.com>
2024-04-25 22:42:13 -0700

    Initial Commit

---
+++ programmticNavBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  programmticNavBar
+//
+//  Created by Alfredo Amezcua on 4/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
